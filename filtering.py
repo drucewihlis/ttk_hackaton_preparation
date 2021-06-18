d50e0f5d44437b420f79093f2a52ff6aea5e266b
Name No <ivan9.6@mail.ru>
2021-06-18 18:51:59 +0300

    'filtering'

---
+++ filtering.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 18 16:46:06 2021
+
+@author: Mi
+"""
+
+from scipy import signal
+import numpy as np
+from logmmse import logmmse_from_file
+import pickle
+from scipy.io import wavfile
+import matplotlib.pyplot as plt 
+
+def filtering(filt, wav='03.03_19.wav'):
+    out = logmmse_from_file(wav)
+    sig_ff = signal.lfilter(filt,[1], out[:,0])
+    return sig_ff
+if __name__ =='__main__':
+    with open('filter.pkl', 'rb') as f:
+        taps, w, h = pickle.load(f)
+
+    wav = '03.03_19.wav'
+    #result = filtering(filt, wav)
+
+    out = filtering(taps, wav)
+
+    plt.plot(out[0:10000])
+    wavfile.write("example.wav", 48000, out)
